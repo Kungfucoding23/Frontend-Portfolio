115010a86e7bb72b7125bae9f249c6e1adb507d1
Alejandro <alesantin012@hotmail.com>
2021-02-02 23:08:32 -0300

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './About.css';
+
+const About = () => {
+    const [info, setInfo] = useState({});
+    //llamada a la API utilizando axios
+    useEffect(() => {
+        //aca despues va la direccion de heroku pero por el momento uso el local
+        axios.get('http://localhost:8080/about')
+        .then(
+            res => setInfo(res.data[0])
+        )}, [setInfo]);
+    return(
+        <div className="About">
+            <div className="div-photo">
+                <img className="profile" alt="ProfilePicture" src={info.photo}></img>
+            </div>
+            <div className="Description">
+                <h1>{info.name}</h1>
+                <h2>{info.profession}</h2>
+                <p>{info.about_me}</p>
+                <ul className="skills">
+                    <h3>Skills:</h3>
+                    {info.skills && info.skills.map((skill, index) => 
+                        <p className="skill" key={index}>{skill}</p>
+                    )}
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+export default About;
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './Portfolio.css';
+
+const Portfolio = () => {
+    const [projects, setProjects] = useState({});
+    //llamada a la API utilizando axios
+    useEffect(() => {
+        //aca despues va la direccion de heroku pero por el momento uso el local
+        axios.get('http://localhost:8080/portfolio')
+        .then(
+            res => setProjects(res.data[0])
+        )}, [setProjects]);
+    return(
+        <>
+            <div className="mega-card-portfolio">
+                {projects.length > 0 && projects.map((project, index) =>                         
+                    <div className="individual-card" key={index}>
+                        <h1>{project.name}</h1>
+                        <p>{project.descripcion}</p>
+                        <img className="project-image" alt="project" src={project.image}></img>
+                        <a className="btn-visit" href={project.link}>Visitar Proyecto</a>
+                    </div>
+                )}
+            </div>
+        </>
+    )
+};
+
+export default Portfolio;
